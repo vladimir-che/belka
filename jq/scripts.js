add97cc907a4d96426924d2a4610ea1397aa9e49
Vladimir <vladimir_che@list.ru>
2017-11-07 22:09:40 +0300

    Добавил бутер + новая стилизация

--- jq/scripts.js
+++ jq/scripts.js
@@ -7,5 +7,21 @@ $(document).ready(function(){
 
   $('.reviews__wrapper').slick();
 
+  $('.header__burger').click(function () {
+    $('.burger_menu').toggleClass('burger_menu__none');
+    });
+
+  $('#age').click(function () {
+    $('.list_age').toggleClass('list__display--flex');
+    });
+
+  $('#place').click(function () {
+    $('.list_place').toggleClass('list__display--flex');
+    });
+
+  $('#duration').click(function () {
+    $('.list_duration').toggleClass('list__display--flex');
+    });
+
 
 });
